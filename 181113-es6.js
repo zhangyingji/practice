138b21ee3ee7e42e056653d50cec72aa20a13ef0
zhangyingji <13134741@qq.com>
2018-12-15 10:21:07 +0800

    update readme

--- 181113-es6.js
+++ 181113-es6.js
@@ -9,4 +9,19 @@ function add(...values){
 }
 
 // console.log(add(1,2,3));
+// console.log(add())
+
+
+// 箭头函数
+var values = [1,2,3];
+var result = values.sort((a,b) => a - b);
+
+// console.log(result);
+
+
+// symbol
+let mySymbol = Symbol('symbol');
+
+// console.log(mySymbol);
+
 
